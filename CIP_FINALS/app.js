b276c440121366a235c9b59f08243205b1dcc59e
dn <69494267+dancypaco@users.noreply.github.com>
2023-05-25 07:38:18 +0800

    Add files via upload

---
+++ CIP_FINALS/app.js
@@ -0,0 +1,127 @@
+let openShopping = document.querySelector('.shopping');
+let closeShopping = document.querySelector('.closeShopping');
+let list = document.querySelector('.list');
+let listCard = document.querySelector('.listCard');
+let body = document.querySelector('body');
+let total = document.querySelector('.total');
+let quantity = document.querySelector('.quantity');
+
+openShopping.addEventListener('click', ()=>{
+    body.classList.add('active');
+})
+closeShopping.addEventListener('click', ()=>{
+    body.classList.remove('active');
+})
+
+let products = [
+    {
+        id: 1,
+        name: 'Lemony Limelight',
+        image: '1.PNG',
+        price: 1200
+    },
+    {
+        id: 2,
+        name: 'Minty Mystique',
+        image: '2.PNG',
+        price: 1500
+    },
+    {
+        id: 3,
+        name: 'Against Fiery Trough',
+        image: '3.PNG',
+        price: 1500
+    },
+    {
+        id: 4,
+        name: 'Island Vacation',
+        image: '4.PNG',
+        price: 1550
+    },
+    {
+        id: 5,
+        name: 'Admiral of the Seven Seas',
+        image: '5.PNG',
+        price: 2000
+    },
+    {
+        id: 6,
+        name: 'Fragrant Perspire',
+        image: '6.PNG',
+        price: 1700
+    },
+    {
+        id: 7,
+        name: 'Thalassophile',
+        image: '7.PNG',
+        price: 1500
+    },
+    {
+        id: 8,
+        name: 'Nature Untamed',
+        image: '8.PNG',
+        price: 1200
+    },
+    {
+        id: 9,
+        name: 'Brave Dive',
+        image: '9.PNG',
+        price: 1500
+    },
+];
+let listCards  = [];
+function initApp(){
+    products.forEach((value, key) =>{
+        let newDiv = document.createElement('div');
+        newDiv.classList.add('item');
+        newDiv.innerHTML = `
+            <img src="image/${value.image}">
+            <div class="title">${value.name}</div>
+            <div class="price">₱${value.price.toLocaleString()}</div>
+            <button onclick="addToCard(${key})">Add To Cart</button>`;
+        list.appendChild(newDiv);
+    })
+}
+initApp();
+function addToCard(key){
+    if(listCards[key] == null){
+        // copy product form list to list card
+        listCards[key] = JSON.parse(JSON.stringify(products[key]));
+        listCards[key].quantity = 1;
+    }
+    reloadCard();
+}
+function reloadCard(){
+    listCard.innerHTML = '';
+    let count = 0;
+    let totalPrice = 0;
+    listCards.forEach((value, key)=>{ 
+        totalPrice = totalPrice + value.price;
+        count = count + value.quantity;
+        if(value != null){
+            let newDiv = document.createElement('li');
+            newDiv.innerHTML = `
+                <div><img src="image/${value.image}"/></div>
+                <div>${value.name}</div>
+                <div>${value.price.toLocaleString()}</div>
+                <div>
+                    <button onclick="changeQuantity(${key}, ${value.quantity - 1})">-</button>
+
+                    <div class="count">${value.quantity}</div>
+                    <button onclick="changeQuantity(${key}, ${value.quantity + 1})">+</button>
+                </div>`;
+                listCard.appendChild(newDiv);
+        }
+    })
+    total.innerText = totalPrice.toLocaleString();
+    quantity.innerText = count;
+}
+function changeQuantity(key, quantity){
+    if(quantity == 0){
+        delete listCards[key];
+    }else{
+        listCards[key].quantity = quantity;
+        listCards[key].price = quantity * products[key].price;
+    }
+    reloadCard();
+}
